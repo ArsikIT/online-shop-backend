a18ef412e55deb7be6471f28fda67e7f7a47495f
ArsikIT <arslan.zheksenbayev@mail.ru>
2026-02-03 22:50:47 +0500

    feature-ArsikIT: Order

--- config/db.js
+++ config/db.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
  */
 const connectDB = async () => {
   try {
-    // В Mongoose 6, 7, 8 и 9 объект с опциями больше не нужен
     const conn = await mongoose.connect(process.env.MONGODB_URI);
 
     console.log(` MongoDB Connected: ${conn.connection.host}`);
---
+++ src/routes/order.routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const orderController = require("../controllers/order.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
+const roleMiddleware = require("../middlewares/role.middleware");
+
+router.use(authMiddleware);
+
+// user
+router.post("/", orderController.createOrder);
+router.get("/my", orderController.getMyOrders);
+
+// admin
+router.get(
+  "/",
+  roleMiddleware("admin"),
+  orderController.getAllOrders
+);
+
+module.exports = router;
